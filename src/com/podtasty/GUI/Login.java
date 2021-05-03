ad2fefa6f183e256c9a4d7a076574c8b69209d6b
Amine Douiri <amindouiri99@gmail.com>
2021-05-04 00:06:31 +0100

    first commit

---
+++ src/com/podtasty/GUI/Login.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.podtasty.GUI;
+
+/**
+ * GUI builder created Form
+ *
+ * @author Douiri Amine
+ */
+public class Login extends com.codename1.ui.Form {
+
+    public Login() {
+        this(com.codename1.ui.util.Resources.getGlobalResources());
+    }
+    
+    public Login(com.codename1.ui.util.Resources resourceObjectInstance) {
+        initGuiBuilderComponents(resourceObjectInstance);
+    }
+
+////-- DON'T EDIT BELOW THIS LINE!!!
+
+
+// <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initGuiBuilderComponents(com.codename1.ui.util.Resources resourceObjectInstance) {
+        setLayout(new com.codename1.ui.layouts.LayeredLayout());
+        setInlineStylesTheme(resourceObjectInstance);
+        setScrollableX(true);
+        setScrollableY(true);
+                setInlineStylesTheme(resourceObjectInstance);
+        setTitle("Login");
+        setName("Login");
+    }// </editor-fold>
+
+//-- DON'T EDIT ABOVE THIS LINE!!!
+}
